5bf61b6132db0fc3790ea52e7f57ee7c5c82ce1c
Anthony Howell <anthony@howell-it.com>
2020-01-24 15:09:46 -0800

    added more content

---
+++ 2020-01-30 - MTX Portland - REST APIs/Demos/03 - Interpreting Curl Syntax.ps1
@@ -0,0 +1,60 @@
+# Getting items from Airtable
+$token = Get-Content C:\users\AnthonyHowell\Documents\at.txt
+## curl
+curl https://api.airtable.com/v0/appTczXUIAllL0x88/Work%20Items \
+-H "Authorization: Bearer YOUR_API_KEY"
+
+## PowerShell
+$headers = @{
+    Authorization = "Bearer $token"
+}
+Invoke-RestMethod -Uri 'https://api.airtable.com/v0/appTczXUIAllL0x88/Work%20Items' -Headers $headers
+
+# Creating items in Airtable
+## curl
+curl -v -X POST https://api.airtable.com/v0/appTczXUIAllL0x88/Clients \
+  -H "Authorization: Bearer YOUR_API_KEY" \
+  -H "Content-Type: application/json" \
+  --data '{
+  "records": [
+    {
+      "fields": {
+        "Work Items": [
+          "reczyGwJ6AiXVq9TA"
+        ],
+        "Name": "South Lake Store"
+      }
+    },
+    {
+      "fields": {
+        "Work Items": [
+          "reczwzBP3ygLPraYu",
+          "recFFxoFVGrMPtbSE",
+          "recGKApP4vqYZpeZI"
+        ],
+        "Name": "Marina Shop"
+      }
+    }
+  ]
+}'
+
+## PowerShell
+$headers = @{
+    Authorization = "Bearer $token"
+    'Content-Type' = 'application/json'
+}
+
+$body = @{
+    records = @(
+        fields = @{
+            workitems = @("reczwzBP3ygLPraYu","recFFxoFVGrMPtbSE","recGKApP4vqYZpeZI")
+            Name = 'Marina Shop'
+        },
+        @{
+            workitems = @("reczyGwJ6AiXVq9TA")
+            Name = 'South Lake Store'
+        }
+    )
+} | ConvertTo-Json
+
+Invoke-RestMethod -Uri 'https://api.airtable.com/v0/appTczXUIAllL0x88/Clients' -Headers $headers -Body $body
