b92c49d3e3932358e781d6478dcfc94f1835d1bd
Anthony Howell <anthony@howell-it.com>
2020-01-26 14:29:26 -0800

    added more content

--- 2020-01-30 - MTX Portland - REST APIs/Demos/03 - Interpreting Curl Syntax.ps1
+++ 2020-01-30 - MTX Portland - REST APIs/Demos/03 - Interpreting Curl Syntax.ps1
@@ -1,6 +1,8 @@
 # Getting items from Airtable
 $token = Get-Content C:\users\AnthonyHowell\Documents\at.txt
 ## curl
+# link: https://airtable.com/appTczXUIAllL0x88/api/docs#curl/table:work%20items:list
+# this link is specific to an Airtable 'base'. This is using a template base.
 curl https://api.airtable.com/v0/appTczXUIAllL0x88/Work%20Items -H "Authorization: Bearer YOUR_API_KEY"
 
 ## PowerShell
@@ -11,6 +13,7 @@ Invoke-RestMethod -Uri 'https://api.airtable.com/v0/appTczXUIAllL0x88/Work%20Ite
 
 # Creating items in Airtable
 ## curl
+# link: https://airtable.com/appTczXUIAllL0x88/api/docs#curl/table:clients:create
 curl -v -X POST https://api.airtable.com/v0/appTczXUIAllL0x88/Clients \
   -H "Authorization: Bearer YOUR_API_KEY" \
   -H "Content-Type: application/json" \
@@ -56,11 +59,33 @@ $body = @{
     )
 } | ConvertTo-Json
 
-Invoke-RestMethod -Uri 'https://api.airtable.com/v0/appTczXUIAllL0x88/Clients' -Headers $headers -Body $body
+Invoke-RestMethod -Method Post -Uri 'https://api.airtable.com/v0/appTczXUIAllL0x88/Clients' -Headers $headers -Body $body
+
+# Basic Authentication
+# link: https://documenter.getpostman.com/view/4454237/apisherpadeskcom-playground/RW8AooQg?version=latest#6a1f8cfa-8910-8c9f-2e68-bfaefb51920b
+
+## curl
+curl --request GET 'https://ncg1in-8d1rag:5nuauzj5pkfftlz3fmyksmyhat6j35kf@api.sherpadesk.com/tickets?status=open,onhold&role=user&limit=6&format=json'
+
+## PowerShell
+$auth = Get-Content C:\users\AnthonyHowell\Documents\sd.txt | ConvertFrom-Json
+# using their values
+$encodedAuth = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes('ncg1in-8d1rag:5nuauzj5pkfftlz3fmyksmyhat6j35kf'))
+# using actual values
+$encodedAuth = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$($auth.username):$($auth.password)"))
+$headers = @{
+    Authorization = "Basic $encodedAuth"
+    Accept = 'application/json'
+}
+Invoke-RestMethod -Uri 'https://api.sherpadesk.com/tickets?status=open,onhold&role=user&limit=6&format=json' -Headers $headers
 
 # Curl2PS
+## curl man pages: https://curl.haxx.se/docs/manpage.html
+
 ## Import dev version
 Import-Module ..\Curl2PS\build\Curl2PS
 
 ## Convert
 ConvertTo-IRM 'curl https://api.airtable.com/v0/appTczXUIAllL0x88/Work%20Items -H "Authorization: Bearer YOUR_API_KEY"' -String
+
+ConvertTo-IRM "curl --request GET 'https://ncg1in-8d1rag:5nuauzj5pkfftlz3fmyksmyhat6j35kf@api.sherpadesk.com/tickets?status=open,onhold&role=user&limit=6&format=json'" -String
