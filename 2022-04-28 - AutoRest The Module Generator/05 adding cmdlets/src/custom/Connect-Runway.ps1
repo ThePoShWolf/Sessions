f4bfecc422955b21ac2253d3a5e745bbc3ea7bbc
Anthony Howell <anthony@howell-it.com>
2022-04-17 14:56:43 -0700

    adding more stuff

--- 2022-04-28 - AutoRest The Module Generator/05 adding cmdlets/src/custom/Connect-Runway.ps1
+++ 2022-04-28 - AutoRest The Module Generator/05 adding cmdlets/src/custom/Connect-Runway.ps1
@@ -2,14 +2,15 @@ Function Connect-Runway {
     [cmdletbinding()]
     [OutputType()]
     param (
-        [Parameter(Mandatory)]
+        [Parameter(
+            Position = 0,
+            Mandatory
+        )]
         [string]$Email,
         [Parameter(Mandatory)]
         [SecureString]$Password,
         [string]$RunwayDomain = 'portal.runway.host'
     )
-    $env:RunwayDomain = $RunwayDomain
-
     $s = Invoke-RwLoginAuthentication -Email $Email -Password ([pscredential]::new('blah',$Password).GetNetworkCredential().Password) -Remember
     $env:RunwaySessionToken = $s.Session
 }
