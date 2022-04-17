15638a19f00adf0c55bdd321f16ea1e6eebed204
Anthony Howell <anthony@howell-it.com>
2022-04-17 06:56:46 -0700

    adding stuff to build module

---
+++ 2022-04-28 - AutoRest The Module Generator/03 build/src/custom/Connect-Runway.ps1
@@ -0,0 +1,15 @@
+Function Connect-Runway {
+    [cmdletbinding()]
+    [OutputType()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$Email,
+        [Parameter(Mandatory)]
+        [SecureString]$Password,
+        [string]$RunwayDomain = 'portal.runway.host'
+    )
+    $env:RunwayDomain = $RunwayDomain
+
+    $s = Invoke-RwLoginAuthentication -Email $Email -Password ([pscredential]::new('blah',$Password).GetNetworkCredential().Password) -Remember
+    $env:RunwaySessionToken = $s.Session
+}
