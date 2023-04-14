2f298b6689bef9d003e35745e3221103737366a7
Anthony Howell <anthony@howell-it.com>
2023-04-13 19:02:25 -0700

    adding filter left examples

---
+++ 2023-04-27 - Making Kubectl PowerShell friendly/03 - Filter Left/01 - Filter Left.ps1
@@ -0,0 +1,60 @@
+#region Pods
+
+## Get running pods
+### Where-Object
+k get pods | Where-Object { $_.Status -eq 'Running' }
+
+### Kubectl
+kubectl get pods --field-selector=status.phase=Running
+
+
+## Get non-running pods
+### Where-Object
+k get pods | Where-Object { $_.Status -ne 'Running' }
+
+### Kubectl
+kubectl get pods --field-selector=status.phase!=Running
+
+
+## Finding field selector labels
+$pods = (k get pods -o json | ConvertFrom-Json).Items
+$pods[0].Status.Phase
+
+
+## Get pod by app
+### Where-Object
+### No reasonable way without -o json. Maybe filter on name?
+k get pods | Where-Object { $_.Name -like '*myapp*' }
+
+### Kubectl
+kubectl get pods -l app=myapp
+
+
+## Get pod by namespace
+### Where-Object
+### No reasonable way without -o json.
+
+### kubectl
+kubectl get pods -n mynamespace
+
+
+## Only return pod name
+### specialK
+(k get pods -l app=myapp --field-selector status.phase=Running | Where-Object { $_.Status -eq 'Running' }).name
+
+### kubectl
+kubectl get pods -l app=myapp --field-selector status.phase=Running -o=jsonpath='{.items..metadata.name}'
+
+#endregion
+
+
+#region Nodes
+
+## Get ready nodes
+### Where-Object
+k get nodes | Where-Object { $_.Status -eq 'Ready' }
+
+### Kubectl
+### I'm not aware of solution without awk/grep
+
+#endregion
