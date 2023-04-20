7baab9e8a0d32464eac24a48d0d11db5c6e60319
Anthony Howell <anthony@howell-it.com>
2023-04-19 20:27:27 -0700

    adding some more supporting code

--- 2023-04-27 - Making Kubectl PowerShell friendly/01 - Parsing/01 - Sample.ps1
+++ 2023-04-27 - Making Kubectl PowerShell friendly/01 - Parsing/01 - Sample.ps1
@@ -5,8 +5,17 @@ $out = @(
     'web-deployment-98ad9380dd-8brg3         1/1      Running    0           2d7h'
 )
 
+# If my local kubernetes cluster is working...
+$out = kubectl get pod
+$out
+$out.GetType()
+$out.Count
+
+# headers
+$out[0]
+
 # if the output starts with the typical headers
-if ($out[0] -match '^(NAME|CURRENT)') {
+if ($out[0] -match '^(NAME |NAMESPACE |CURRENT |LAST SEEN )') {
     # locate all positions to place semicolons
     # we are using the headers since some values may be null in the data
     if ($null -ne $out) {
@@ -15,9 +24,9 @@ if ($out[0] -match '^(NAME|CURRENT)') {
 
     # view matches
     $m
-    $m.Matches
+    $m.Matches | Format-Table
 
-    # place semicolons
+    # place semicolons, order descending
     $out = foreach ($line in $out) {
         foreach ($index in ($m.Matches.Index | Sort-Object -Descending)) {
             $line = $line.Insert($index + 2, ';')
@@ -28,6 +37,8 @@ if ($out[0] -match '^(NAME|CURRENT)') {
     # view semicolons
     $out
 
+    # objectCommands is the formats.json hashtable
+    # will explain when we get to formats
     if ($objectCommands[$args[0]] -contains $args[1]) {
         # select the format type name
         # dynamically determined in the script
