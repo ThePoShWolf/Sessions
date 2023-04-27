e57d4c1591f641d26c845dd7274247666b260d4d
Anthony Howell <anthony@howell-it.com>
2023-04-26 17:05:39 -0700

    adding additional commands used in demo

--- 2023-04-26 - Building a serverless Discord bot/04 - Manage Bot Commands/01 - Add Commands.ps1
+++ 2023-04-26 - Building a serverless Discord bot/04 - Manage Bot Commands/01 - Add Commands.ps1
@@ -1,12 +1,17 @@
 # Auth to discord using bot token
-Connect-Discord -RestClient -TokenType Bot -Token 'insert bot token here'
+Connect-Discord -RestClient -TokenType Bot -Token (Get-Content C:\tmp\bt.txt)
 
 # Get the guild where you want the commands
 $guild = Get-DiscordGuild | Where-Object { $_.Name -eq 'Bot Testing' }
+$guild
 
 # Add a basic command
 New-DiscordGuildCommand -Name 'hello' -Description "Say hello to the bot" -Guild $guild -CommandBuilder (
     New-DiscordSlashCommand -Name 'hello' -Description 'Say Hello to the bot'
+)
+
+New-DiscordGuildCommand -Name 'bye' -Description "Say hello to the bot" -Guild $guild -CommandBuilder (
+    New-DiscordSlashCommand -Name 'by' -Description 'Say Hello to the bot'
 )
 
 # Add a command with parameters
@@ -15,3 +20,7 @@ New-DiscordGuildCommand -Name 'start-server' -Description 'Start a server' -Guil
         (New-DiscordSlashCommandOption -Name 'server' -Description 'The selected server' -Type String)
     )
 )
+
+# Send a command to a channel
+$channel = Get-DiscordChannel | Where-Object { $_.Name -eq 'general' }
+Send-DiscordMessage -GuildId $guild.Id -ChannelId $channel.Id -MessageText 'Hello!'
