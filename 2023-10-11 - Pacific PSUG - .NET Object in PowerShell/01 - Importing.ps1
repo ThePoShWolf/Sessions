91021ae2d0ab0b1db071653edd600119f5edece2
Anthony Howell <anthony@howell-it.com>
2023-09-26 19:48:40 -0700

    adding initial files

---
+++ 2023-10-11 - Pacific PSUG - .NET Object in PowerShell/01 - Importing.ps1
@@ -0,0 +1,13 @@
+# Very simple
+
+Add-Type -Path "Fully qualified path to .dll"
+
+# For example, if you had the Discord.NET .dlls:
+
+Get-Item ..\Discordant\lib\*.dll | ForEach-Object {
+    Add-Type -Path $_.FullName
+}
+
+# If you are importing into a module, you can instead use RequiredAssemblies in the manifest
+# Show Modrify build
+code ..\Modrify\build\Modrify
