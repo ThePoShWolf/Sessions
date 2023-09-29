2cef52941472dedb035c00ddec4cdcc73089a89a
Anthony Howell <anthony@howell-it.com>
2023-09-28 19:53:58 -0700

    adding gettypes

--- 2023-10-11 - Pacific PSUG - .NET Object in PowerShell/00 - Understanding Types.ps1
+++ 2023-10-11 - Pacific PSUG - .NET Object in PowerShell/00 - Understanding Types.ps1
@@ -2,28 +2,41 @@
 
 # string
 "string"
+"string".GetType()
 
 # integer (int32)
 42
+$answer = 42
+$answer.GetType()
 
 # boolean
 $true
+$true.GetType()
 
 # DateTime
 Get-Date
+(Get-Date).GetType()
 
 # hashtable (dictionary)
 @{
     key = "value"
 }
+@{
+    key = "value"
+}.GetType()
 
 # PSCustomObject
 [pscustomobject]@{
     Property = "Value"
 }
+$obj = [pscustomobject]@{
+    Property = "Value"
+}
+$obj.GetType()
 
 # Example of a built-in .NET type
 (Get-Process)[0]
+(Get-Process)[0].GetType()
 
 # How to get an object's type
 
