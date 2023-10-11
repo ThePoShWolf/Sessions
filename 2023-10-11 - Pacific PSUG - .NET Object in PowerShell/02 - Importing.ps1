efc02a864ac04df9a70f1074907e46179951861c
Anthony Howell <anthony@howell-it.com>
2023-10-11 07:47:42 -0700

    minor updates

--- 2023-10-11 - Pacific PSUG - .NET Object in PowerShell/02 - Importing.ps1
+++ 2023-10-11 - Pacific PSUG - .NET Object in PowerShell/02 - Importing.ps1
@@ -5,12 +5,18 @@ Add-Type -Path "Fully qualified path to .dll"
 # For example, if you had the Discord.NET .dlls:
 
 Get-Item ..\Discordant\lib\*.dll | ForEach-Object {
+    Add-Type $_.FullName
+}
+
+# Don't forget -Path
+
+Get-Item ..\Discordant\lib\*.dll | ForEach-Object {
     Add-Type -Path $_.FullName
 }
 
 # If you are importing into a module, you can instead use RequiredAssemblies in the manifest
 # Show Modrify build
 
-code ..\Modrify\build\Modrify
+code ..\Modrify\build\Modrify\Modrify.psd1
 
 # Both ways make the .NET types available to the shell, not just module cmdlets
