558ae1ddef8f1d8c6754ccf8a34797a7f5d650f5
Anthony Howell <anthony@howell-it.com>
2023-10-10 19:52:10 -0700

    adding some more stuff

--- 2023-10-11 - Pacific PSUG - .NET Object in PowerShell/05 - Generics.ps1
+++ 2023-10-11 - Pacific PSUG - .NET Object in PowerShell/05 - Generics.ps1
@@ -20,7 +20,18 @@ $arr.GetType().FullName
 # Methods
 
 [System.Object[]]::new
+
+# Generic add
 $arr.Add
+
+# How are a string, integer, and datetime all System.Object?
+# Polymorphism: https://en.wikipedia.org/wiki/Polymorphism_(computer_science)
+
+$arr[0].GetType().BaseType
+$arr[1].GetType().BaseType
+$arr[2].GetType().BaseType
+
+$client.GetType().BaseType
 
 # Hashtable
 # https://learn.microsoft.com/en-us/dotnet/api/system.collections.hashtable?view=net-7.0
@@ -36,6 +47,9 @@ $ht.GetType().FullName
 [System.Collections.Hashtable]::new
 $ht.Add
 
+# Crazy example
+$ht.Add($client, $arr)
+
 # List
 # https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1?view=net-7.0
 # List<T>
@@ -44,3 +58,4 @@ $ht.Add
 [System.Collections.Generic.List[string]]::new()
 [System.Collections.Generic.List[int]]::new()
 [System.Collections.Generic.List[Discord.Rest.DiscordRestClient]]::new()
+[System.Collections.Generic.List[System.Object]]::new()
