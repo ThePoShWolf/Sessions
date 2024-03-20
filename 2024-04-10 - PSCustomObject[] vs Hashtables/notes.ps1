a3f478e814309be65dfad7b063a73479eb0254a3
Anthony Howell <anthony@howell-it.com>
2024-03-20 11:48:54 -0700

    correcting aliases

--- 2024-04-10 - PSCustomObject[] vs Hashtables/notes.ps1
+++ 2024-04-10 - PSCustomObject[] vs Hashtables/notes.ps1
@@ -1,27 +1,32 @@
 $ht = @{}
-0..10000 | % { $ht[(new-guid).Guid] = @{'blah' = (new-guid).Guid } }
+0..10000 | ForEach-Object { $ht[(new-guid).Guid] = @{'blah' = (new-guid).Guid } }
 
 $key = $ht.Keys | Select-Object -first 1
 Measure-Command {
-    0..100 | % { $ht.keys -contains $key }
+    0..100 | ForEach-Object { $ht.keys -contains $key }
 }
 
 $key = $ht.Keys | Select-Object -Last 1
 Measure-Command {
-    0..100 | % { $ht.keys -contains $key }
+    0..100 | ForEach-Object { $ht.keys -contains $key }
 }
 
-$arr = 0..10000 | % { [pscustomobject]@{
+$arr = 0..10000 | ForEach-Object { [pscustomobject]@{
         id   = (new-guid).Guid
         blah = (new-guid).Guid
     } }
 
 $id = $arr[0].id
 Measure-Command {
-    0..100 | % { $arr | ? { $_.id -eq $id } | Select-Object -first 1 }
+    0..100 | ForEach-Object { $arr | Where-Object { $_.id -eq $id } | Select-Object -first 1 }
+}
+
+$id = $arr[5000].id
+Measure-Command {
+    0..100 | ForEach-Object { $arr | Where-Object { $_.id -eq $id } | Select-Object -first 1 }
 }
 
 $id = $arr[10000].id
 Measure-Command {
-    0..100 | % { $arr | ? { $_.id -eq $id } | Select-Object -first 1 }
+    0..100 | ForEach-Object { $arr | Where-Object { $_.id -eq $id } | Select-Object -first 1 }
 }
