bed91b749383ae0582696a7f00575f9b1fbe55d2
Anthony Howell <anthony@howell-it.com>
2024-03-20 11:16:48 -0700

    more ht examples

--- 2024-04-10 - PSCustomObject[] vs Hashtables/01 - Creating/Hashtable.ps1
+++ 2024-04-10 - PSCustomObject[] vs Hashtables/01 - Creating/Hashtable.ps1
@@ -28,5 +28,77 @@ $ht = [ordered]@{
 }
 $ht
 
+# Nested
+$ht = @{
+    Event    = 'PowerShell Summit 2024'
+    Date     = @{
+        Year  = 2024
+        Month = 4
+        Day   = 10
+    }
+    Location = @{
+        City  = 'Bellevue'
+        State = 'WA'
+    }
+}
+
+# Large hashtable
+$ht = @{
+    '2024-04-10' = @{
+        Event    = 'PowerShell Summit 2024'
+        Date     = @{
+            Year  = 2024
+            Month = 4
+            Day   = 10
+        }
+        Location = @{
+            City  = 'Bellevue'
+            State = 'WA'
+        }
+    }
+    '2024-04-11' = @{
+        Event    = 'PowerShell Summit 2024'
+        Date     = @{
+            Year  = 2024
+            Month = 4
+            Day   = 11
+        }
+        Location = @{
+            City  = 'Bellevue'
+            State = 'WA'
+        }
+    }
+}
+
+# As an array
+$ht = @{
+    Events = @(
+        @{
+            Event    = 'PowerShell Summit 2024'
+            Date     = @{
+                Year  = 2024
+                Month = 4
+                Day   = 10
+            }
+            Location = @{
+                City  = 'Bellevue'
+                State = 'WA'
+            }
+        },
+        @{
+            Event    = 'PowerShell Summit 2024'
+            Date     = @{
+                Year  = 2024
+                Month = 4
+                Day   = 11
+            }
+            Location = @{
+                City  = 'Bellevue'
+                State = 'WA'
+            }
+        }
+    )
+}
+
 # Cleanup
 Remove-Variable ht
