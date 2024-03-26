2c5e8c5335ee04359539220239fa12e01b1d0e3e
Anthony Howell <anthony@howell-it.com>
2024-03-26 07:33:51 -0700

    adding hashtable updating stuff

--- 2024-04-10 - PSCustomObject[] vs Hashtables/02 - Update/Hashtable.ps1
+++ 2024-04-10 - PSCustomObject[] vs Hashtables/02 - Update/Hashtable.ps1
@@ -1,9 +1,43 @@
-# Update property
+# Update property using hashtable notation
+$ht['Location'] = 'Redmond, WA'
+$ht
 
-# Add property
+# Update property using dot notation
+$ht.Location = 'Seattle, WA'
+$ht
+
+# Add property using hashtable notation
+$ht['MilitaryTime'] = '0900'
+$ht
+
+# Add property using dot notation
+$ht.Time = '9:00 AM'
+$ht
+
+# Add property using Add method
+$ht.Add('TimeZone', 'PST')
+$ht
 
 # Remove property
+$ht.Remove('TimeZone')
+$ht
 
 # Finding properties
+$ht.Keys
+
+# Testing for a property
+$ht.ContainsKey('Location')
+$ht.Keys -contains 'Location'
+
+# Which is more efficient?
+Measure-Command {
+    0..10000 | ForEach-Object { $ht.ContainsKey('Location') }
+}
+Measure-Command {
+    0..10000 | ForEach-Object { $ht.Keys -contains 'Location' }
+}
 
 # Accessing a property from a variable
+$property = 'Location'
+$ht.$property
+$ht[$property]
