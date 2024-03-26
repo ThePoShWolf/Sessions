4f45b5ba280a455938fd9d2eb186355b34909471
Anthony Howell <anthony@howell-it.com>
2024-03-26 07:50:36 -0700

    adding update info for pscustomobject

--- 2024-04-10 - PSCustomObject[] vs Hashtables/02 - Update/PSCustomObject.ps1
+++ 2024-04-10 - PSCustomObject[] vs Hashtables/02 - Update/PSCustomObject.ps1
@@ -1,9 +1,33 @@
-# Update property
+# Update property using dot notation
+$obj.Location = 'Seattle, WA'
+$obj
 
-# Add property
+# Add property using Add-Member
+$obj | Add-Member -MemberType NoteProperty -Name MilitaryTime -Value '0900'
+$obj
+
+# Add property using Add-Member with a hashtable
+$obj | Add-Member -NotePropertyMembers @{
+    TimeZone = 'PST'
+    Time     = '9:00 AM'
+}
+$obj
+
+# Add property using Add() method
+$obj.PSObject.Properties.Add([PSNoteProperty]::new('Address', '123 Main st'))
+$obj
 
 # Remove property
+$obj.PSOject.Properties.Remove('Address')
+$obj
 
 # Finding properties
+$obj.PSObject.Properties.Name
+
+# Testing for a property
+$obj.PSObject.Properties.Name -contains 'Location'
 
 # Accessing a property from a variable
+$property = 'Location'
+$obj.$property
+$obj[$property]
