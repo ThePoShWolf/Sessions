9a3c734cdf586c6ca44f2f0712bfc2a38f21a93c
Anthony Howell <anthony@howell-it.com>
2024-03-28 14:11:10 -0700

    adding other iterators

--- 2024-04-10 - PSCustomObject[] vs Hashtables/04 - Iterate/PSCustomObject.ps1
+++ 2024-04-10 - PSCustomObject[] vs Hashtables/04 - Iterate/PSCustomObject.ps1
@@ -1,6 +1,29 @@
-# Normal foreach
+# Foreach
 foreach ($item in $objs) {
     "Event: $($item.Event)"
     "Date: $($item.Date)"
     "Location: $($item.Location)"
+}
+
+# ForEach-Object
+$objs | ForEach-Object {
+    "Event: $($_.Event)"
+    "Date: $($_.Date)"
+    "Location: $($_.Location)"
+}
+
+# For
+for ($x = 0; $x -lt $objs.Count; $x++) {
+    "Event: $($objs[$x].Event)"
+    "Date: $($objs[$x].Date)"
+    "Location: $($objs[$x].Location)"
+}
+
+# While
+$x = 0
+while ($x -lt $objs.Count) {
+    "Event: $($objs[$x].Event)"
+    "Date: $($objs[$x].Date)"
+    "Location: $($objs[$x].Location)"
+    $x++
 }
