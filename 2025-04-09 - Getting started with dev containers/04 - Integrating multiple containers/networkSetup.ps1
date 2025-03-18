fc768ef7c69b9a5409346d9ab67244b2c32511ec
Anthony Howell <anthony@howell-it.com>
2025-03-18 15:01:23 -0700

    variablize network name

--- 2025-04-09 - Getting started with dev containers/04 - Integrating multiple containers/networkSetup.ps1
+++ 2025-04-09 - Getting started with dev containers/04 - Integrating multiple containers/networkSetup.ps1
@@ -1,16 +1,17 @@
 # input the values of each container
-$apiContainer = ''
-$psContainer = ''
+$apiContainer = '0f95b4a6ec7c'
+$psContainer = 'a1c69da7174e'
+$networkName = 'pssummit'
 
 # create the network
-docker network create pssummit
+docker network create $networkName
 
 # connect both containers to the network
-docker network connect pssummit $apiContainer
-docker network connect pssummit $psContainer
+docker network connect $networkName $apiContainer
+docker network connect $networkName $psContainer
 
 # examine the network
-docker network inspect pssummit
+docker network inspect $networkName
 
 # from inside the container, make an API call
 Invoke-RestMethod "http://$apiContainer`:8080/helloWorld"
