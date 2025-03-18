b102cf078b673b40014868898f71eb14f8c524d4
Anthony Howell <anthony@howell-it.com>
2025-03-18 14:16:43 -0700

    switching up go example to be a simple API

---
+++ 2025-04-09 - Getting started with dev containers/03 - Adding PowerShell to config/go.mod
@@ -0,0 +1,3 @@
+module theposhwolf/helloWorld
+
+go 1.23.4
--- 2025-04-09 - Getting started with dev containers/03 - Adding PowerShell to config/main.go
+++ 2025-04-09 - Getting started with dev containers/03 - Adding PowerShell to config/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloWorld(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("/helloWorld called")
+	fmt.Fprintf(w, "Hello World\n")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/helloWorld", helloWorld)
+	fmt.Println("Server launching on port 80...")
+	http.ListenAndServe(":8080", nil)
 }
